b4ebfe9f5816e85df1244be5154338b34ed4e044
magicpieh28 <choi.jung_won.ca1@is.naist.jp>
2020-06-21 17:08:23 +0900

    14. input n via prompt

--- chapter2/14.py
+++ chapter2/14.py
@@ -1,13 +1,14 @@
 # print nth record
+import os
 from pathlib import Path
 
 
-def select_record(txt_file: Path, n: int):
+def select_record(txt_file: Path):
     with txt_file.open(mode="r") as txt:
-        selected = txt.readlines()[n - 1]
+        selected = txt.readlines()[int(input("input which record you want: ")) - 1]
     return selected
 
 
 if __name__ == '__main__':
     txt_file = Path(__file__).resolve().parent / 'hightemp.txt'
-    print(select_record(txt_file, 24))
+    print(select_record(txt_file))
