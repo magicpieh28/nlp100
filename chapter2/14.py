ae5cc4eb1e4d01cd5464fccbde19da1029128639
magicpieh28 <choi.jung_won.ca1@is.naist.jp>
2020-06-21 16:54:45 +0900

    14 select record

--- chapter2/14.py
+++ chapter2/14.py
@@ -1,8 +1,8 @@
-# print nth column
+# print nth record
 from pathlib import Path
 
 
-def select_column(txt_file: Path, n: int):
+def select_record(txt_file: Path, n: int):
     with txt_file.open(mode="r") as txt:
         selected = txt.readlines()[n - 1]
     return selected
@@ -10,4 +10,4 @@ def select_column(txt_file: Path, n: int):
 
 if __name__ == '__main__':
     txt_file = Path(__file__).resolve().parent / 'hightemp.txt'
-    print(select_column(txt_file, 24))
+    print(select_record(txt_file, 24))
